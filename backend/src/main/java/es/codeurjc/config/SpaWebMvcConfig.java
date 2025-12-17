9458d2de2b846db514a4e7f2609939a0fbb118d3
danielmunmar <d.munozm.2022@alumnos.urjc.es>
2025-12-17 19:19:33 +0100

    Configure Dockerfile to serve Angular app as static resource in Spring backend over HTTPS

---
+++ backend/src/main/java/es/codeurjc/config/SpaWebMvcConfig.java
@@ -0,0 +1,17 @@
+package es.codeurjc.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpaWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+        
+        registry.addViewController("/{path1:[^\\.]*}/{path2:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    }
+}
